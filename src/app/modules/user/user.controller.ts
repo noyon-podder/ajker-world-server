0516cf0d0b5b2d259889cf0db70fc6c786490ae8
noyon-podder <noyon.podder7@gmail.com>
2024-05-31 02:15:37 +0600

    catch async utils function created

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,19 +1,15 @@
-import { NextFunction, Request, Response } from 'express'
 import { UserServices } from './user.service'
+import catchAsync from '../../utils/catchAsync'
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const result = await UserServices.createUserIntoDB(req.body)
+const createUser = catchAsync(async (req, res) => {
+  const result = await UserServices.createUserIntoDB(req.body)
 
-    res.status(200).json({
-      success: true,
-      message: 'User create successfully!!',
-      data: result,
-    })
-  } catch (err) {
-    next(err)
-  }
-}
+  res.status(200).json({
+    success: true,
+    message: 'User create successfully!!',
+    data: result,
+  })
+})
 
 export const UserControllers = {
   createUser,
