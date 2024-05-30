ce1f4c9bd94b6b4c6fa662b3f45305c96ace19cb
noyon-podder <noyon.podder7@gmail.com>
2024-05-31 02:26:55 +0600

    send response utils function created

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,12 +1,14 @@
 import { UserServices } from './user.service'
 import catchAsync from '../../utils/catchAsync'
+import sendResponse from '../../utils/sendResponse'
 
 const createUser = catchAsync(async (req, res) => {
   const result = await UserServices.createUserIntoDB(req.body)
 
-  res.status(200).json({
+  sendResponse(res, {
+    statusCode: 200,
     success: true,
-    message: 'User create successfully!!',
+    message: 'User Create Successfully!',
     data: result,
   })
 })
